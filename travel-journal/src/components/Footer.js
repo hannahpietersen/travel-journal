2865d00448acb4545c69b474eb15f4f1196e2e21
someid <hannahpietersen@gmail.com>
2023-02-27 18:49:45 +0530

    added code to respective files

---
+++ travel-journal/src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        Coded by{" "}
+        Hannah Kongara
+      </small>
+    </footer>
+  );
+}
---
+++ travel-journal/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+export default function Navbar() {
+  return (
+    <nav>
+      
+      {/* <a className="nav--link-home" href="#" rel="noopener noreferrer"> */}
+      <h1 className="nav--title">my travel journal.</h1>
+      {/* </a> */}
+    </nav>
+  );
+}
